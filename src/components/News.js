17ce8ec93ab9ad4d473cd256b7e9263f947c29d1
himalaypuri05 <himalaypuri05@gmail.com>
2026-03-24 08:06:39 +0530

    Fix ESLint issue properly

--- src/components/News.js
+++ src/components/News.js
@@ -4,15 +4,14 @@ import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const News = (props) => {
-
+const News = props => {
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [hasMore, setHasMore] = useState(true);
   const [totalResults, setTotalResults] = useState(0);
 
-  const capitalizeFirstLetter = (string) => {
+  const capitalizeFirstLetter = string => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
@@ -38,14 +37,14 @@ const News = (props) => {
     props.setProgress(100);
   };
 
-// eslint-disable-next-line react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
-  document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
-  updateNews();
-}, [props.category]); // important
+    document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
+    updateNews();
+  }, [props.category]); // important
 
   const fetchMoreData = async () => {
-
     if (articles.length >= totalResults) {
       setHasMore(false);
       return;
@@ -65,9 +64,7 @@ const News = (props) => {
 
     setPage(nextPage);
 
-    setArticles(prev =>
-      prev.concat(parsedData.articles)
-    );
+    setArticles(prev => prev.concat(parsedData.articles));
   };
 
   return (
@@ -78,15 +75,10 @@ const News = (props) => {
 
       {loading && <Spinner />}
 
-      <InfiniteScroll
-        dataLength={articles.length}
-        next={fetchMoreData}
-        hasMore={hasMore}
-        loader={<Spinner />}
-      >
+      <InfiniteScroll dataLength={articles.length} next={fetchMoreData} hasMore={hasMore} loader={<Spinner />}>
         <div className="container">
           <div className="row">
-            {articles.map((element) => (
+            {articles.map(element => (
               <div className="col-md-4" key={element.url}>
                 <NewsItems
                   title={element.title ? element.title.slice(0, 45) : ""}
@@ -109,13 +101,13 @@ const News = (props) => {
 News.defaultProps = {
   country: "us",
   pageSize: 15,
-  category: "general",
+  category: "general"
 };
 
 News.propTypes = {
   country: PropTypes.string,
   pageSize: PropTypes.number,
-  category: PropTypes.string,
+  category: PropTypes.string
 };
 
 export default News;
