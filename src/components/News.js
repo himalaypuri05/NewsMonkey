f3fc404cba9d173c6800ea396dfc1c1deb60b12f
himalaypuri05 <himalaypuri05@gmail.com>
2026-05-10 22:25:26 +0530

    Switched to AllOrigins proxy

--- src/components/News.js
+++ src/components/News.js
@@ -4,14 +4,8 @@ import Spinner from "./Spinner";
 import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-const News = ({
-  country = "us",
-  pageSize = 15,
-  category = "general",
-  apiKey,
-  setProgress,
-}) => {
-  const capitalizeFirstLetter = (string) => {
+const News = ({ country = "us", pageSize = 15, category = "general", apiKey, setProgress }) => {
+  const capitalizeFirstLetter = string => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
@@ -30,8 +24,9 @@ const News = ({
 
       // ================= GNEWS API WITH CORS PROXY =================
 
-      const url = `https://corsproxy.io/?https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=${country}&max=${pageSize}&apikey=${apiKey}`;
-
+      const url = `https://api.allorigins.win/raw?url=${encodeURIComponent(
+        `https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=${country}&max=${pageSize}&apikey=${apiKey}`
+      )}`;
       // ================= NEWSAPI =================
 
       // const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${page}&pageSize=${pageSize}`;
@@ -59,9 +54,7 @@ const News = ({
   };
 
   useEffect(() => {
-    document.title = `${capitalizeFirstLetter(
-      category
-    )} - NewsMonkey`;
+    document.title = `${capitalizeFirstLetter(category)} - NewsMonkey`;
 
     updateNews();
 
@@ -78,8 +71,9 @@ const News = ({
 
       // ================= GNEWS API WITH CORS PROXY =================
 
-      const url = `https://corsproxy.io/?https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=${country}&max=${pageSize}&page=${nextPage}&apikey=${apiKey}`;
-
+      const url = `https://api.allorigins.win/raw?url=${encodeURIComponent(
+        `https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&country=${country}&max=${pageSize}&page=${nextPage}&apikey=${apiKey}`
+      )}`;
       // ================= NEWSAPI =================
 
       // const url = `https://newsapi.org/v2/top-headlines?country=${country}&category=${category}&apiKey=${apiKey}&page=${nextPage}&pageSize=${pageSize}`;
@@ -89,9 +83,7 @@ const News = ({
       let parsedData = await data.json();
 
       if (parsedData.articles) {
-        setArticles((prevArticles) =>
-          prevArticles.concat(parsedData.articles)
-        );
+        setArticles(prevArticles => prevArticles.concat(parsedData.articles));
       }
 
       setTotalResults(parsedData.totalArticles || 0);
@@ -102,10 +94,7 @@ const News = ({
 
   return (
     <>
-      <h1
-        className="text-center"
-        style={{ margin: "35px 0px", marginTop: "90px" }}
-      >
+      <h1 className="text-center" style={{ margin: "35px 0px", marginTop: "90px" }}>
         NewsMonkey - Top {capitalizeFirstLetter(category)} Headlines
       </h1>
 
@@ -119,29 +108,15 @@ const News = ({
       >
         <div className="container">
           <div className="row">
-            {articles.map((element) => {
+            {articles.map(element => {
               return (
                 <div className="col-md-4 my-3" key={element.url}>
                   <NewsItems
-                    title={
-                      element.title
-                        ? element.title.slice(0, 45)
-                        : ""
-                    }
-                    description={
-                      element.description
-                        ? element.description.slice(0, 88)
-                        : ""
-                    }
-
+                    title={element.title ? element.title.slice(0, 45) : ""}
+                    description={element.description ? element.description.slice(0, 88) : ""}
                     // ================= GNEWS IMAGE =================
 
-                    imageUrl={
-                      element.image
-                        ? element.image
-                        : "https://via.placeholder.com/300x180.png?text=No+Image"
-                    }
-
+                    imageUrl={element.image ? element.image : "https://via.placeholder.com/300x180.png?text=No+Image"}
                     // ================= NEWSAPI IMAGE =================
 
                     // imageUrl={
@@ -168,7 +143,7 @@ const News = ({
 News.propTypes = {
   country: PropTypes.string,
   pageSize: PropTypes.number,
-  category: PropTypes.string,
+  category: PropTypes.string
 };
 
 export default News;
